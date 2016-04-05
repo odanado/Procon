e3aae5428719eeb0eb85fcc758a101c1da833c7c
odan <odan3240@gmail.com>
2016-04-06 00:17:12 +0900

    Solve ABC013 D

--- AtCoder/ABC013/D.cpp
+++ AtCoder/ABC013/D.cpp
@@ -33,17 +33,39 @@ typedef long long ll;
 
 int N,M,D;
 int A[200005];
-int amida[200005];
+vector<int> amida;
+void print(const vector<int> &vec) {
+    for(auto e:vec) cout<<e<<" ";
+    cout<<endl;
+}
+vector<int> mul(const vector<int> &vec1, const vector<int> &vec2) {
+    vector<int> vec(vec1.size());
+
+    rep(i,vec1.size()) {
+        vec[i]=vec1[vec2[i]];
+    }
+
+    return vec;
+}
+vector<int> power(vector<int> vec,int n) {
+    vector<int> ret(vec.size());
+    rep(i,ret.size()) ret[i]=i;
+
+    while(n) {
+        if(n&1) ret=mul(ret,vec);
+        vec=mul(vec,vec);
+        n>>=1;
+    }
+    return ret;
+}
 int main() {
     cin>>N>>M>>D;
     rep(i,M) cin>>A[i];
-    rep(i,200005) amida[i]=i;
-    rep(i,M) swap(amida[A[i]-1],amida[A[i]]);
+    amida.resize(max(M,N)+1);
+    rep(i,amida.size()) amida[i]=i;
 
-    int cnt = 0;
-    rep(i,N) if(amida[i]==i) cnt++;
-    D = D % (N-cnt);
-    rep(i,D-1) rep(i,M) swap(amida[A[i]-1],amida[A[i]]);
+    rep(i,M) swap(amida[A[i]-1],amida[A[i]]);
+    amida=power(amida, D);
     vector<int> ans;
     ans.resize(N);
     rep(i,N) {
