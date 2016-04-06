91dc535734f9dddee70dcd1550931dbff9a64417
odan <odan3240@gmail.com>
2016-04-06 13:47:19 +0900

    Solve ABC022 D

--- AtCoder/ABC022/D.cpp
+++ AtCoder/ABC022/D.cpp
@@ -1,83 +1,69 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <map>
+#include <set>
+#include <queue>
 #include <algorithm>
-#include <functional>
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <string>
-#include <sstream>
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <list>
-#include <stack>
-#include <queue>
-#include <map>
-#include <set>
-#include <bitset>
-#include <climits>
+
+using namespace std;
+using ll = long long;
 
 #define all(c) (c).begin(), (c).end()
-#define rep(i,n) for(int i=0;i<(n);i++)
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define pb(e) push_back(e)
 #define mp(a, b) make_pair(a, b)
 #define fr first
 #define sc second
 
-const int INF=100000000;
-int dx[4]={1,0,-1,0};
-int dy[4]={0,1,0,-1};
-using namespace std;
-typedef long long ll;
-const double PI = acos(-1.0);
+const ll INF = 1e9;
+const ll MOD = 1e9 + 7;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
 
-struct P {
-    double x,y;
-    double eps;
-    P(double x=0,double y=0) :
-        x(x),y(y){}
-    void shift(double r) {
-        x+=r;
-        y+=r;
-        eps=1e-8;
+pair<double, double> calcGravity(vector<pair<double, double>> &vec) {
+    pair<double, double> ret;
+    for (auto e : vec) {
+        ret.first += e.first;
+        ret.second += e.second;
     }
+    ret.first /= vec.size();
+    ret.second /= vec.size();
+    return ret;
+}
 
-    void rot(double theta) {
-        double t=theta*PI/180.0;
-        double xx=x;
-        double yy=y;
-        x=xx*cos(t)-yy*sin(t);
-        y=xx*sin(t)+yy*cos(t);
+double calcDist(pair<double, double> p1, pair<double, double> p2) {
+    return sqrt(pow(p1.first - p2.first, 2) + pow(p1.second - p2.second, 2));
+}
+
+double calcNearestDist(vector<pair<double, double>> &vec,
+                       pair<double, double> p) {
+    double ret = calcDist(vec.front(), p);
+    for (auto e : vec) {
+        ret = max(ret, calcDist(e, p));
     }
+    return ret;
+}
 
-    void mul(double p) {
-        x*=p;
-        y*=p;
-    }
-
-    void serialize() {
-        if(x<eps) x=0;
-        if(y<eps) y=0;
-    }
-
-    bool operator==(const P& rth) const {
-        return x==rth.x && y==rth.y;
-    }
-
-
-};
-
-int n;
-int Ax[100005];
-int Ay[100005];
-int Bx[100005];
-int By[100005];
-
+int N;
+vector<pair<double, double>> A, B;
 int main() {
-    cin>>n;
-    rep(i,n) cin>>Ax[i]>>Ay[i];
-    rep(i,n) cin>>Bx[i]>>By[i];
+    cin >> N;
+    A.resize(N);
+    B.resize(N);
+    rep(i, N) cin >> A[i].first >> A[i].second;
+    rep(i, N) cin >> B[i].first >> B[i].second;
+    auto g1 = calcGravity(A);
+    auto g2 = calcGravity(B);
+
+    auto ans = calcNearestDist(B, g2) / calcNearestDist(A, g1);
+
+    printf("%.12f\n", ans);
 
     return 0;
 }
-
