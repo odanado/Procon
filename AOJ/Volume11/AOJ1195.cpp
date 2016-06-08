acb25dd5122dabcabe8e69e2483e926e9c140d63
odan <odan3240@gmail.com>
2016-06-08 22:05:22 +0900

    Solve AOJ1195

--- AOJ/Volume11/AOJ1195.cpp
+++ AOJ/Volume11/AOJ1195.cpp
@@ -1,88 +1,63 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <map>
+#include <set>
+#include <queue>
 #include <algorithm>
-#include <functional>
 #include <cmath>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <string>
-#include <sstream>
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <list>
-#include <stack>
-#include <queue>
-#include <map>
-#include <set>
-#include <bitset>
-#include <climits>
- 
+
+using namespace std;
+using ll = long long;
+
 #define all(c) (c).begin(), (c).end()
-#define rep(i,n) for(int i=0;i<(n);i++)
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define pb(e) push_back(e)
 #define mp(a, b) make_pair(a, b)
 #define fr first
 #define sc second
- 
-const int INF=100000000;
-int dx[4]={1,0,-1,0};
-int dy[4]={0,1,0,-1};
-using namespace std;
-typedef pair<int ,int > P;
-typedef long long ll;
- 
+
+const ll INF = 1e9;
+const ll MOD = 1e9 + 7;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
+
 string s;
-vector<string> vec;
- 
-void func(int idx,int S) {
-    if(s.size()==idx) {
-        vec.pb(s);
+vector<string> ans;
+void dfs(int idx, int S) {
+    if (idx == s.size()) {
+        ans.push_back(s);
         return;
     }
- 
-    int p=s[idx]-'a';
-    if(s[idx]!='z' && !((S>>(p+1))&1)) {
-        s[idx]++;
-        func(idx+1,S|(1<<p));
-        s[idx]--;
+    int t = s[idx] - 'a';
+    if (S >> t & 1) {
+        dfs(idx + 1, S);
     }
-    if((S>>p)&1) func(idx+1,S);
- 
+    if (!(S >> (t + 1) & 1)) {
+        if (t != 25) {
+            s[idx]++;
+            dfs(idx + 1, S | 1 << (t + 1));
+            s[idx]--;
+        }
+    }
 }
- 
-void solve() {
-    string t(s);
-    vec.clear();
-    func(0,0);
 
-    sort(all(vec));
-
-    cout<<vec.size()<<endl;
-    if(vec.size()<=10) {
-        for(auto &e : vec) {
-            cout<<e<<endl;
-        }
-    }
-    else {
-        auto it = vec.begin();
-        rep(i,5) {
-            cout<<*it<<endl;
-            ++it;
-        }
-        it = vec.end();
-        rep(i,5) {
-            --it;
-            cout<<*it<<endl;
-        }
-    }
- 
-}
- 
 int main() {
-    while(cin>>s) {
-        if(s=="#") break;
-        solve();
- 
+    while (cin >> s) {
+        if (s == "#") break;
+        ans.clear();
+        dfs(0, 1);
+        sort(all(ans));
+        cout << ans.size() << endl;
+        if (ans.size() > 10) {
+            rep(i, 5) cout << ans[i] << endl;
+            rep(i, 5) cout << ans[ans.size() + i - 5] << endl;
+        } else
+            for (auto e : ans) cout << e << endl;
     }
     return 0;
 }
